36aa0b96b7c1b94d8ada6876a863ada8af08ece3
Chan Kim <104743574+wannabechan@users.noreply.github.com>
2026-02-02 00:30:34 +0900

    Add files via upload

--- api/orders/my.js
+++ api/orders/my.js
@@ -8,7 +8,7 @@ const { getOrdersByUser } = require('../_redis');
 
 const STATUS_LABELS = {
   submitted: '주문 신청 완료',
-  payment_link_issued: '결제 링크 발급',
+  payment_link_issued: '결제 링크 보기',
   payment_completed: '결제 완료',
   delivery_completed: '배송 완료',
   cancelled: '주문 취소',
